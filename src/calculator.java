7094d4657d00f707d73d853c14adefd2f6d261ce
miatroiano <m11ssing1n4ction@gmail.com>
2022-02-07 21:37:39 -0500

    two numbers added

--- src/calculator.java
+++ src/calculator.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 
 public class calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("enter one number.");
+		int num = scanner.nextInt();
+		System.out.println("enter another number.");
+		int num2 = scanner.nextInt();
+		System.out.println(num + num2);
 
 	}
 
