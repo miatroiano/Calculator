5dccd483dd151601cee11fca16ffd5e6d4e4dbb4
miatroiano <m11ssing1n4ction@gmail.com>
2022-02-09 16:59:05 -0500

    double/floats

--- src/calculator.java
+++ src/calculator.java
@@ -9,8 +9,10 @@ public class calculator {
 		
 		System.out.println("would you like to add,subtract, multiply, or divide your numbers?");
 		String type = scanner.nextLine();
-		
-	
+		System.out.println("are your numbers Ints, Doubles, or Floats");
+		String type2 = scanner.nextLine();
+
+		if (type2.equalsIgnoreCase("ints")) {
 			System.out.println("enter one number.");
 			int num = scanner.nextInt();
 			System.out.println("enter another number.");
@@ -31,6 +33,55 @@ public class calculator {
 				System.out.println(num / num2);
 				
 			}
+		}
+
+		else if (type2.equalsIgnoreCase("doubles")) {
+			System.out.println("enter one number.");
+			Double num = scanner.nextDouble();
+			System.out.println("enter another number.");
+			Double num2 = scanner.nextDouble();
+
+			if (type.equalsIgnoreCase("add")) {
+				System.out.println(num + num2);
+			}
+			else if (type.equalsIgnoreCase("subtract")) {
+				System.out.println(num - num2);
+				
+			}
+			else if (type.equalsIgnoreCase("multiply")) {
+				System.out.println(num * num2);
+				
+			}
+			else if (type.equalsIgnoreCase("divide")) {
+				System.out.println(num / num2);
+				
+			}
+		}
+
+		else if (type2.equalsIgnoreCase("floats")) {
+			System.out.println("enter one number.");
+			Float num = scanner.nextFloat();
+			System.out.println("enter another number.");
+			Float num2 = scanner.nextFloat();
+
+			if (type.equalsIgnoreCase("add")) {
+				System.out.println(num + num2);
+			}
+			else if (type.equalsIgnoreCase("subtract")) {
+				System.out.println(num - num2);
+				
+			}
+			else if (type.equalsIgnoreCase("multiply")) {
+				System.out.println(num * num2);
+				
+			}
+			else if (type.equalsIgnoreCase("divide")) {
+				System.out.println(num / num2);
+				
+			}
+				
+			}
+
 		}
 
 }
